5ec1074293f53c916417ef26027994b37363e670
longhodac-ucsd <lhodac@ucsd.edu>
2025-05-13 20:27:13 -0700

    finished

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -60,8 +60,7 @@ function saveRecipesToStorage(recipes) {
 	// B1. TODO - Complete the functionality as described in this function
 	//            header. It is possible in only a single line, but should
 	//            be no more than a few lines.
-	recipesString = JSON.stringify(recipes);
-	localStorage.s('recipes', recipesString);
+	localStorage.setItem('recipes', JSON.stringify(recipes));
 }
 
 /**
@@ -75,8 +74,7 @@ function initFormHandler() {
 	//            submit button is clicked
 	form.addEventListener('submit', (event) => {
 		event.preventDefault();
-	});
-	// Steps B4-B9 will occur inside the event listener from step B3
+		// Steps B4-B9 will occur inside the event listener from step B3
 	// B4. TODO - Create a new FormData object from the <form> element reference above
 	const formData = new FormData(form);
 	// B5. TODO - Create an empty object (we'll refer to this object as recipeObject to
@@ -95,20 +93,20 @@ function initFormHandler() {
 	main.appendChild(recipeCard);
 	// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
 	//            then save the recipes array back to localStorage
-	let recipes = getRecipesFromStorage();
+	const recipes = getRecipesFromStorage();
 	recipes.push(recipeObject);
 	saveRecipesToStorage(recipes);
+	});
+	
 	// B10. TODO - Get a reference to the "Clear Local Storage" button
-	const clearButton = document.getElementsByClassName('danger');
+	const clearButton = document.querySelector('.danger');
 	// B11. TODO - Add a click event listener to clear local storage button
 	clearButton.addEventListener('click', (event) => {
 		event.preventDefault();
+	// B12. TODO - Clear the local storage
+		localStorage.clear();
+	// B13. TODO - Delete the contents of <main>
+		document.querySelector('main').innerHTML ='';
 	});
 	// Steps B12 & B13 will occur inside the event listener from step B11
-	// B12. TODO - Clear the local storage
-	localStorage.clear();
-	// B13. TODO - Delete the contents of <main>
-	while (main.firstChild) {
-		main.removeChild(main.firstChild);
-	}
 }
